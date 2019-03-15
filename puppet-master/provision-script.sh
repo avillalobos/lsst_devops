60fbb39205a740361aeb1cb06146df26356ebdd6
Andres Villalobose <aevr007@gmail.com>
2019-03-15 12:47:41 -0300

    ENVIRONMENT variable used only for puppet code

--- puppet-master/provision-script.sh
+++ puppet-master/provision-script.sh
@@ -7,7 +7,7 @@ rpm -Uvh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
 yum install -y puppetserver git
 sed -i 's#\-Xms2g#\-Xms512m#g;s#\-Xmx2g#\-Xmx512m#g' /etc/sysconfig/puppetserver
 echo -e "certname = puppet-master.dev.lsst.org" >> /etc/puppetlabs/puppet/puppet.conf
-echo -e "\n[main]\nenvironment = ${ENVIRONMENT}" >> /etc/puppetlabs/puppet/puppet.conf
+echo -e "\n[main]\nenvironment = production" >> /etc/puppetlabs/puppet/puppet.conf
 echo -e "\n[agent]\nserver = puppet-master.dev.lsst.org" >> /etc/puppetlabs/puppet/puppet.conf
 echo -e "${IP}\tpuppet-master\tpuppet-master.dev.lsst.org" > /etc/hosts
 
@@ -24,10 +24,9 @@ then
 	mkdir -p /etc/puppetlabs/code/hieradata/production
 fi
 
-
-if [ -z "$(grep etc_puppetlabs_code_hieradata_$ENVIRONMENT /etc/fstab)" ]
+if [ -z "$(grep etc_puppetlabs_code_hieradata_production /etc/fstab)" ]
 then
-  echo "etc_puppetlabs_code_hieradata_$ENVIRONMENT /etc/puppetlabs/code/hieradata/$ENVIRONMENT vboxsf defaults,ro 0 0" >> /etc/fstab
+echo "etc_puppetlabs_code_hieradata_production /etc/puppetlabs/code/hieradata/production vboxsf defaults,ro 0 0" >> /etc/fstab
 	mount -a
 fi
 
@@ -48,7 +47,7 @@ do
 	echo "Found environment $f"
 	if [ ! -d "/etc/puppetlabs/code/hieradata/$f" ]
 	then
-		echo "dir $f don't exists, creating it from develop"
+		echo "dir $f don't exists, creating it from production"
 		ln -s /etc/puppetlabs/code/hieradata/production /etc/puppetlabs/code/hieradata/$f
 	fi
 done
@@ -58,11 +57,17 @@ then
 	mkdir /opt/puppet-code
 fi
 
-if [ -z "$(grep opt_puppet-code /etc/fstab)" ]
+# If there is a shared folder for the puppet code
+if [ ! -z "$(VBoxControl sharedfolder list | grep opt_puppet-code)" ]
 then
-  echo "opt_puppet-code /opt/puppet-code vboxfs defaults,ro 0 0" >> /etc/fstab
-	echo "/opt/puppet-code /etc/puppetlabs/code/environments/$ENVIRONMENT none defaults,ro,bind 0 0" >> /etc/fstab
-  mount -a
+	# if the sharefolder is not in the fstab yet
+	if [ -z "$(grep opt_puppet-code /etc/fstab)" ]
+	then
+		echo "Shared puppet code dir found, mounted into: /etc/puppetlabs/code/environments/$ENVIRONMENT"
+		echo "opt_puppet-code /opt/puppet-code vboxfs defaults,ro 0 0" >> /etc/fstab
+		echo "/opt/puppet-code /etc/puppetlabs/code/environments/$ENVIRONMENT none defaults,ro,bind 0 0" >> /etc/fstab
+		mount -a
+	fi
 fi
 
 if [ ! -f /etc/puppetlabs/puppet/autosign.conf ]
